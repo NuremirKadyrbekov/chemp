69c21e73748a803153bb95173316ba63218aeafd
azimust <woody.azimbek@gmail.com>
2024-05-31 21:02:12 +0600

    feat: add sectiontwo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import Navigation from "./components/Header/Navigation/Navigation";
 import SectionOne from "./components/SectionOne/SectionOne";
+import SectionTwo from "./components/SectionTwo/SectionTwo";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SectionOne />
+      <SectionTwo />
     </div>
   );
 }
---
+++ src/components/SectionTwo/SectionTwo.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./SectionTwo.css";
+import code from '../../sources/images/Card.png'
+
+const SectionTwo = () => {
+  return (
+    <div className="sectiontwo">
+      <div className="container">
+        <div className="sectiontwo__body">
+          <div className="sectiontwo__desc">
+            <div className="sectiontwo__title">
+              Самые мощные и <br /> простые в <br /> использовании API <br /> в мире
+            </div>
+            <div className="sectiontwo__subdesc">
+              <div className="sectiontwo__subtitle">
+                Инструменты для каждого стека
+              </div>
+              <p className="sectiontwo__text">
+                Мы предлагаем клиентские и серверные <br /> библиотеки на всех языках -
+                от React и PHP <br /> до .NET и iOS.
+              </p>
+            </div>
+            <div className="sectiontwo__subdesc">
+              <div className="sectiontwo__subtitle">Готовые интеграции</div>
+              <p className="sectiontwo__text">
+                Используйте интеграцию с такими <br /> системами, как Shopify,
+                WooCommerce, <br /> NetSuite и другими.
+              </p>
+            </div>
+          </div>
+          <div className="sectiontwo__image">
+            <img src={code} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SectionTwo;
